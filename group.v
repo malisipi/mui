a33486c57aa4b5d3ebfc0a68a81bfc8e6229f8b0
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2023-08-29 22:00:05 +0300

    Support rtl layout on group

--- group.v
+++ group.v
@@ -32,13 +32,25 @@ fn draw_group(app &Window, object map[string]WindowData){
 		text_width, text_height:=app.gg.text_size(object["text"].str)
 		app.gg.draw_rounded_rect_filled(object["x"].num, object["y"].num, object["w"].num, object["h"].num, app.round_corners, object["bg"].clr)
 		app.gg.draw_rounded_rect_empty(object["x"].num+10, object["y"].num+10, object["w"].num-20, object["h"].num-20, app.round_corners, object["bfg"].clr)
-		app.gg.draw_rect_filled(object["x"].num+20, object["y"].num, text_width+4, 20, object["bg"].clr)
 
-		app.gg.draw_text(object["x"].num+22, object["y"].num+text_height/2+2, object["text"].str, gx.TextCfg{
-			color: object["fg"].clr
-			size: 20
-			align: .left
-			vertical_align: .middle
-		})
+		if !app.rtl_layout { // LTR
+			app.gg.draw_rect_filled(object["x"].num+20, object["y"].num, text_width+4, 20, object["bg"].clr)
+
+			app.gg.draw_text(object["x"].num+22, object["y"].num+text_height/2+2, object["text"].str, gx.TextCfg{
+				color: object["fg"].clr
+				size: 20
+				align: .left
+				vertical_align: .middle
+			})
+		} else { // RTL
+			app.gg.draw_rect_filled(object["x"].num+object["w"].num-text_width-24, object["y"].num, text_width+4, 20, object["bg"].clr)
+
+			app.gg.draw_text(object["x"].num+object["w"].num-22, object["y"].num+text_height/2+2, object["text"].str, gx.TextCfg{
+				color: object["fg"].clr
+				size: 20
+				align: .right
+				vertical_align: .middle
+			})
+		}
 	}
 }
